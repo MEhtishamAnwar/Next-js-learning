f9e0e4f295e1df80d14391f99a2b74c1102df114
MEhtishamAnwar <mehtishamanwar52@gmail.com>
2026-08-30 22:38:27 +0500

    understanding thr folder structure

---
+++ my-app/src/app/page.js
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+<> 
+<h1 className="font-bold text-3xl justify-center flex bg-gray-500 text-black">Well  Come to Next Js.</h1>
+
+
+</>
+  );
+}
